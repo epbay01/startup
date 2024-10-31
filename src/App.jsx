66aa1d60ba1779a5334d1389d59522f76c15b0da
Elijah <epbay.01@gmail.com>
2024-10-30 18:36:05 -0600

    added new files with stubs

--- src/App.jsx
+++ src/App.jsx
@@ -6,5 +6,14 @@ import "./profile-style.css";
 import "./vote-style.css";
 
 export default function App() {
-    return <div className="body">hi</div>
+    return <div className="body">
+        <header>
+            <img id="logo" src="/Images/vote together_white.png"/>
+            <nav>
+                <a id="nav-1" class="link" href="profile.html">Profile</a>
+                <a id="nav-2" class="link" href="vote.html">VOTE!</a>
+            </nav>
+        </header>
+        <main></main>
+    </div>
 }
