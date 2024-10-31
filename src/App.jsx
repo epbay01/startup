fd0b51c18a34eb6520abd48d3de13da3f87b1dba
Elijah <epbay.01@gmail.com>
2024-10-30 18:24:01 -0600

    reinstalled packages and rearranged things

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./style.css";
+import "./login-style.css";
+import "./profile-style.css";
+import "./vote-style.css";
 
 export default function App() {
     return <div className="body">hi</div>
